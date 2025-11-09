641e9d722d46934e778863202d1ee5e08279801f
Mabrouk Mahdhi <coding@mahdhi.com>
2025-11-09 17:32:33 +0100

    INFRA: Initialize Project

---
+++ Talk1-Balzor-Tools/DemoEFxceptions/DemoEFxceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿// ----------------------------------------------------
+// Copyright (c) Mabrouk Mahdhi. All rights reserved.
+// Made with ❤️ for Update Conference Prague 2025.
+// ----------------------------------------------------
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DemoEFxceptions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder webApplicationBuilder =
+                WebApplication.CreateBuilder(args);
+
+            webApplicationBuilder.Services.AddControllers();
+            webApplicationBuilder.Services.AddOpenApi();
+
+            WebApplication webApplication =
+                webApplicationBuilder.Build();
+
+            if (webApplication.Environment.IsDevelopment())
+            {
+                webApplication.MapOpenApi();
+            }
+
+            webApplication.UseHttpsRedirection();
+            webApplication.UseAuthorization();
+            webApplication.MapControllers();
+            webApplication.Run();
+        }
+    }
+}
